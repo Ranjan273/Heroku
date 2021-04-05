dba3413871474427aa37acbf09434be1f9a2ede9
Supi <ranjanjyoti273@gmail.com>
2021-04-05 06:45:57 +0530

    Adding 2 lines of code in Test one and commiting test2 in BranchJyoti

--- src/test/java/HerokuApp/TestCheckbox.java
+++ src/test/java/HerokuApp/TestCheckbox.java
@@ -2,12 +2,16 @@ package HerokuApp;
 
 import java.util.concurrent.TimeUnit;
 
+import javax.xml.xpath.XPath;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TestWebsite {
+public class TestCheckbox {
 
 	public static void main(String[] args) {
+		
 		System.setProperty("webdriver.chrome.driver","D:\\Chromedriver Latest\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.manage().deleteAllCookies();
@@ -15,10 +19,9 @@ public class TestWebsite {
 		//driver.manage().window().maximize();
 		
 		driver.get("https://the-internet.herokuapp.com/");
+		driver.findElement(By.xpath("//ul/li[6]/a[text()='Checkboxes']")).click();
 		
-		String Title=driver.getTitle();
-		System.out.println(Title);
-		
+		System.out.println(driver.getTitle());
 
 	}
 
